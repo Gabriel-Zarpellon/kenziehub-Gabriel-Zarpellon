9291f1562d6755a38a07e6002b11f1e5b3ab9bce
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-10 14:22:30 -0300

    feat: add input component

---
+++ src/components/FormInput/index.jsx
@@ -0,0 +1,11 @@
+import { useForm } from "react-hook-form"
+
+export function FormInput({name, label, type, placeholder}){
+    let { register } = useForm();
+    return(
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <input type={type} placeholder={placeholder} {...register(`${name}`)} required/>
+        </div>
+    )
+}
