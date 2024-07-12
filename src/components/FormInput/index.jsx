78f03ab0c51aea67aa4b9b12b5dea21533477274
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-12 15:11:54 -0300

    fix: indentation

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -1,11 +1,13 @@
-import { useForm } from "react-hook-form"
-
-export function FormInput({name, label, type, placeholder, register}){
-    
-    return(
-        <div>
-            <label htmlFor={name}>{label}</label>
-            <input type={type} placeholder={placeholder}  {...register(`${name}`)} required/>
-        </div>
-    )
+export function FormInput({ name, label, type, placeholder, register }) {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        {...register(`${name}`)}
+        required
+      />
+    </div>
+  );
 }
