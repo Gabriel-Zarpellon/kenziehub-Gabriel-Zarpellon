d3bf99210f19f907fd0a3486cb081e76c5408b42
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-12 16:25:21 -0300

    feat: add tech edit function

--- src/components/TechList/TechCard/index.jsx
+++ src/components/TechList/TechCard/index.jsx
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import { TechContext } from "../../../providers/TechContext";
 
 export function TechCard({ tech }) {
-  let { deleteTech } = useContext(TechContext);
+  let { deleteTech, setEditTech } = useContext(TechContext);
   return (
     <li>
       <div>
         <h3>{tech.title}</h3>
         <div>
           <p>{tech.status}</p>
-          <button>/</button>
+          <button onClick={() => setEditTech(tech)}>/</button>
           <button onClick={()=> deleteTech(tech.id)}>X</button>
         </div>
       </div>
