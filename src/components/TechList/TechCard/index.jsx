360d8878845463d1930212abf1b2978cd4549482
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-15 17:28:54 -0300

    style: add style module for tech cards

--- src/components/TechList/TechCard/index.jsx
+++ src/components/TechList/TechCard/index.jsx
@@ -1,16 +1,19 @@
 import { useContext } from "react";
 import { TechContext } from "../../../providers/TechContext";
+import styles from "./style.module.scss";
+import { ImPencil } from "react-icons/im";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 export function TechCard({ tech }) {
   let { deleteTech, setEditTech } = useContext(TechContext);
   return (
-    <li>
+    <li className={styles.cardBox}>
+      <h3 className="title3">{tech.title}</h3>
       <div>
-        <h3 className="title3">{tech.title}</h3>
-        <div>
-          <p className="paragraph">{tech.status}</p>
-          <button onClick={() => setEditTech(tech)}>/</button>
-          <button onClick={()=> deleteTech(tech.id)}>X</button>
+        <p className="paragraph">{tech.status}</p>
+        <div className={styles.buttonBox}>
+          <button onClick={() => setEditTech(tech)}><ImPencil size={16}/></button>
+          <button onClick={() => deleteTech(tech.id)}><FaRegTrashAlt size={16}/></button>
         </div>
       </div>
     </li>
