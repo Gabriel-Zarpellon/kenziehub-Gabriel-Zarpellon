2d0e22e9153720f8781bd05c9bc089f9996f7fd3
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-15 17:29:11 -0300

    style: add style module for tech list

--- src/components/TechList/index.jsx
+++ src/components/TechList/index.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { TechContext } from "../../providers/TechContext";
 import { TechCard } from "./TechCard";
+import styles from "./style.module.scss"
 
 export function TechList() {
   let { techList } = useContext(TechContext);
   
   return(
-    <ul>
+    <ul className={`dashboardContainer ${styles.techList}`}>
         {techList.map(tech => <TechCard key={tech.title} tech={tech}/>)}
     </ul>
     
