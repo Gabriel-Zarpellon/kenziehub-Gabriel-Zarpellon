6b8c4f76adb56e6e188e0d2ef503120143f23b79
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-15 15:57:57 -0300

    style: add style module for register form

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useContext } from "react";
 import { UserContext } from "../../providers/UserContext";
+import styles from "./style.module.scss";
 
 export function RegisterPage() {
   const { register, handleSubmit } = useForm();
@@ -17,82 +18,94 @@ export function RegisterPage() {
       alert("As senhas não são iguais!");
     }
   }
+
   return (
-    <section>
-      <div>
-        <img src={Logo} alt="KenzieHub Logo" />
-        <Link to="/">Voltar</Link>
-      </div>
-      <div>
-        <h1 className="title1">Crie sua conta</h1>
-        <p className="paragraph">Rápido e grátis, vamos nessa</p>
-      </div>
-      <form onSubmit={handleSubmit(submit)}>
-        <FormInput
-          name="name"
-          label="Nome"
-          type="text"
-          placeholder="Digite aqui seu nome"
-          register={register}
-        />
-        <FormInput
-          name="email"
-          label="E-mail"
-          type="email"
-          placeholder="Digite aqui seu e-mail"
-          register={register}
-        />
-        <FormInput
-          name="password"
-          label="Senha"
-          type="password"
-          placeholder="Digite aqui sua senha"
-          register={register}
-        />
-        <FormInput
-          name="password2"
-          label="Confirmar Senha"
-          type="password"
-          placeholder="Digite novamente sua senha"
-          register={register}
-        />
-        <FormInput
-          name="bio"
-          label="Bio"
-          type="text"
-          placeholder="Fale sobre você"
-          register={register}
-        />
-        <FormInput
-          name="contact"
-          label="Contato"
-          type="text"
-          placeholder="Opção de contato"
-          register={register}
-        />
-        <div>
-          <label htmlFor="course_module">Selecionar Módulo</label>
-          <select
-            name="course_module"
-            defaultValue={"Primeiro módulo (Introdução ao Frontend)"}
-            {...register("course_module")}
-          >
-            <option value="Primeiro módulo (Introdução ao Frontend)">
-              Primeiro módulo (Introdução ao Frontend)
-            </option>
-            <option value="Segundo módulo (Frontend Avançado)">
-              Segundo módulo (Frontend Avançado)
-            </option>
-            <option value="Terceiro módulo (Introdução ao Backend)">
-              Terceiro módulo (Introdução ao Backend)
-            </option>
-            <option value="Quarto módulo (Backend Avançado)">
-              Quarto módulo (Backend Avançado)
-            </option>
-          </select>
+    <section className={styles.registerSection}>
+      <div className="container">
+        <div className={styles.logoBox}>
+          <img src={Logo} alt="KenzieHub Logo" />
+          <Link to="/">
+            <button>Voltar</button>
+          </Link>
         </div>
-        <button type="submit">Cadastrar</button>
-      </form>
+        <div className={styles.registerBox}>
+          <div className={styles.titleBox}>
+            <h1 className="title1">Crie sua conta</h1>
+            <p className="paragraph">Rápido e grátis, vamos nessa</p>
+          </div>
+          <form className={styles.formBox} onSubmit={handleSubmit(submit)}>
+            <FormInput
+              name="name"
+              label="Nome"
+              type="text"
+              placeholder="Digite aqui seu nome"
+              register={register}
+            />
+            <FormInput
+              name="email"
+              label="E-mail"
+              type="email"
+              placeholder="Digite aqui seu e-mail"
+              register={register}
+            />
+            <FormInput
+              name="password"
+              label="Senha"
+              type="password"
+              placeholder="Digite aqui sua senha"
+              register={register}
+            />
+            <FormInput
+              name="password2"
+              label="Confirmar Senha"
+              type="password"
+              placeholder="Digite novamente sua senha"
+              register={register}
+            />
+            <FormInput
+              name="bio"
+              label="Bio"
+              type="text"
+              placeholder="Fale sobre você"
+              register={register}
+            />
+            <FormInput
+              name="contact"
+              label="Contato"
+              type="text"
+              placeholder="Opção de contato"
+              register={register}
+            />
+            <div className={styles.selectBox}>
+              <label className="label" htmlFor="course_module">
+                Selecionar Módulo
+              </label>
+              <select
+                className="select"
+                name="course_module"
+                defaultValue={"Primeiro módulo (Introdução ao Frontend)"}
+                {...register("course_module")}
+              >
+                <option value="Primeiro módulo (Introdução ao Frontend)">
+                  Primeiro módulo (Introdução ao Frontend)
+                </option>
+                <option value="Segundo módulo (Frontend Avançado)">
+                  Segundo módulo (Frontend Avançado)
+                </option>
+                <option value="Terceiro módulo (Introdução ao Backend)">
+                  Terceiro módulo (Introdução ao Backend)
+                </option>
+                <option value="Quarto módulo (Backend Avançado)">
+                  Quarto módulo (Backend Avançado)
+                </option>
+              </select>
+            </div>
+            <button className="button pink" type="submit">
+              Cadastrar
+            </button>
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
