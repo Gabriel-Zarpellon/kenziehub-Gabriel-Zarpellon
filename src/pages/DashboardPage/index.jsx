8bca4f70347c1ca425eac605ba9045f5fd91999a
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-07-15 17:28:33 -0300

    style: add style module for dashboard page

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -5,37 +5,46 @@ import { AddTechModal } from "../../components/TechList/AddTechModal";
 import { TechContext } from "../../providers/TechContext";
 import { TechList } from "../../components/TechList";
 import { EditTechModal } from "../../components/TechList/EditTechModal";
+import { IoMdAdd } from "react-icons/io";
+import styles from "./style.module.scss";
 
 export function DashboardPage() {
   let { user, userLogout } = useContext(UserContext);
   let { techList, editTech } = useContext(TechContext);
   let [isAddTechOpen, setIsAddTechOpen] = useState(false);
- 
 
   return (
     <>
       <header>
-        <img src={Logo} alt="KenzieHub Logo" />
-        <button onClick={() => userLogout()}>Sair</button>
+        <div className={`dashboardContainer ${styles.logoBox}`}>
+          <img src={Logo} alt="KenzieHub Logo" />
+          <button onClick={() => userLogout()}>Sair</button>
+        </div>
       </header>
       <main>
-        <section>
-          <h1 className="title1">{user.name}</h1>
-          <p className="paragraph">{user.course_module}</p>
+        <section className={styles.titleSection}>
+          <div className={`dashboardContainer ${styles.titleBox}`}>
+            <h1 className="title1">{user.name}</h1>
+            <p className="paragraph">{user.course_module}</p>
+          </div>
         </section>
         <section>
-          <div>
-            <h2 className="title2">Tecnologias</h2>
-            <button onClick={() => setIsAddTechOpen(true)}>+</button>
-            {techList.length > 0 ? <TechList /> : null}
+          <div className="dashboardContainer">
+            <div className={styles.techTitle}>
+              <h2 className="title2">Tecnologias</h2>
+              <button onClick={() => setIsAddTechOpen(true)}>
+                <IoMdAdd size={20} />
+              </button>
+            </div>
+            <div className={styles.techListBox}>
+              {techList.length > 0 ? <TechList /> : null}
+            </div>
           </div>
         </section>
         {isAddTechOpen ? (
           <AddTechModal setIsAddTechOpen={setIsAddTechOpen} />
         ) : null}
-        {editTech ? (
-          <EditTechModal />
-        ) : null}
+        {editTech ? <EditTechModal /> : null}
       </main>
     </>
   );
